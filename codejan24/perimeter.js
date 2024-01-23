39ec7a17517e5e2ccf749712986d8f6a900f8a38
prettygenius <devprettygenius@gmail.com>
2024-01-23 16:11:07 -0600

    js challenges

---
+++ codejan24/perimeter.js
@@ -0,0 +1,13 @@
+//Create a function that takes length and width and finds the perimeter of a rectangle.
+
+//Examples
+//findPerimeter(6, 7) ➞ 26
+
+//findPerimeter(20, 10) ➞ 60
+
+//findPerimeter(2, 9) ➞ 22
+
+function findPerimeter(length, width) {
+	let rect = 2 * (length + width)
+	return rect
+}
