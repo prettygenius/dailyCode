208bac51606d858b5b5e503047aa1c85509b6a7e
prettygenius <devprettygenius@gmail.com>
2024-01-28 18:56:46 -0600

    js challenge

---
+++ codejan24/easyloop.js
@@ -0,0 +1,13 @@
+// Code a for loop that runs through all numbers from 0-99 and append all numbers divisible by seven into a string. Return this string.
+
+function easyLoop( ) {
+    let result = "";
+      for ( let i = 0; i <= 99; i++ ) {
+        if (i % 7 === 0) {
+          // Check if the number is divisible by 7
+          result += i;
+      }
+    }
+      return result
+    }
+    
