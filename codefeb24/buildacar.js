8e97c71a834ebbf12737b8621af489fe73542f98
prettygenius <devprettygenius@gmail.com>
2024-02-08 13:29:51 -0600

    js challenge

---
+++ codefeb24/buildacar.js
@@ -0,0 +1,14 @@
+// Create an object with the following characteristics: 4 wheels (named wheels as integer), 100PS power (named power and integer), 3 doors (named doors as integer). Return the built car.
+
+
+
+function buildCar() {
+    const car = {
+      wheels: 4,
+      power: 100,
+      doors: 3
+    };
+    return car;
+  }
+  
+  
