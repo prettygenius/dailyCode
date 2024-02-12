bb64afdfb01699c536ffb6813bebee964a83c4c7
prettygenius <devprettygenius@gmail.com>
2024-02-12 00:41:52 -0600

    js challenge

---
+++ codefeb24/checkme.js
@@ -0,0 +1,8 @@
+// Set the given checkbox (#my-checkbox) checked.
+
+
+function checkMe() {
+     let mycheckbox = document.querySelector('#my-checkbox')
+     mycheckbox.checked = true
+     return mycheckbox
+   }
