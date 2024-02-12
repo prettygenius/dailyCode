7b99bfa064fc93e815d7b95f461be213a255b00c
prettygenius <devprettygenius@gmail.com>
2024-02-11 23:26:48 -0600

    js challenge

---
+++ codefeb24/diff42.js
@@ -0,0 +1,7 @@
+// The parameter n is given. Return the difference of n to the number 42. The number must be positive.
+
+function difference42(n) {
+    let difference = Math.abs(42 - n);
+    return difference;
+  }
+  
