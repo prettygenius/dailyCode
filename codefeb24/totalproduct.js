1ec839d70a8889c74ad1dedb178183b9878b3dc4
prettygenius <devprettygenius@gmail.com>
2024-02-13 17:49:10 -0600

    js challenge

---
+++ codefeb24/totalproduct.js
@@ -0,0 +1,9 @@
+// Two numbers are given a and b. Return their sum. If both numbers are equal, return their product.
+
+function totalProduct(a, b) {
+    if (a === b) {
+      return a * b;
+    } else {
+      return a + b;
+    }
+  }
