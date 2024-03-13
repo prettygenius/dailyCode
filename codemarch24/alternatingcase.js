3ed8d7ea8d2a121cfc55ad76348b013fbd359d8e
prettygenius <devprettygenius@gmail.com>
2024-03-12 19:49:49 -0500

    codewars js

---
+++ codemarch24/alternatingcase.js
@@ -0,0 +1,13 @@
+// Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase.
+
+
+
+String.prototype.toAlternatingCase = function() {
+    return this.split('').map(char => {
+        if (char === char.toUpperCase()) {
+            return char.toLowerCase();
+        } else {
+            return char.toUpperCase();
+        }
+    }).join('');
+};
