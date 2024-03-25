dc2b63be723f85124a130ca98736c3135f857648
prettygenius <devprettygenius@gmail.com>
2024-03-24 20:08:07 -0500

    codewars js

---
+++ codemarch24/pipeproblem.js
@@ -0,0 +1,15 @@
+// Looks like some hoodlum plumber and his brother has been running around and damaging your stages again.
+
+// The pipes connecting your level's stages together need to be fixed before you receive any more complaints.
+
+// The pipes are correct when each pipe after the first is 1 more than the previous one.
+
+// Task
+// Given a list of unique numbers sorted in ascending order, return a new list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
+
+function pipeFix(numbers) {
+    let min = numbers[0];
+    let max = numbers[numbers.length - 1];
+    let newNumbers = Array.from({length: max - min + 1}, (_, i) => min + i);
+    return newNumbers;
+}
