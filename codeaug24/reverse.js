4e59deacc6b95ff869bc75f1ba5a130252529ef2
prettygenius <devprettygenius@gmail.com>
2024-08-02 11:29:11 -0500

    daily code

---
+++ codeaug24/reverse.js
@@ -0,0 +1,10 @@
+// Complete the solution so that it reverses the string passed into it.
+
+// ‘world’  =>  ‘dlrow’
+
+function reverse(word) {
+  return word.split('').reverse().join('');
+}
+
+console.log(reverse('world')); 
+
